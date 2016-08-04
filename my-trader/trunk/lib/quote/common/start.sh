b7d1da9d0151dd576539e0191ff8c8855c020241
centos_vm <wangying@localhost.localdomain>
2016-08-04 20:47:58 +0800

    add start and stop script files for quote_forwarders.

---
+++ my-trader/trunk/lib/quote/common/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# the following codes obtain the directory where this script file locates.
+ this_dir=`pwd`
+ dirname $0|grep "^/" >/dev/null
+ if [ $? -eq 0 ];then
+         this_dir=`dirname $0`
+ else
+         dirname $0|grep "^\." >/dev/null
+         retval=$?
+         if [ $retval -eq 0 ];then
+                 this_dir=`dirname $0|sed "s#^.#$this_dir#"`
+         else
+                 this_dir=`dirname $0|sed "s#^#$this_dir/#"`
+         fi
+ fi
+
+cd $this_dir
+
+ulimit -c unlimited
+export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+nohup ./allexlv1_mktsvc &
