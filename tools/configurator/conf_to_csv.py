5947e1b9cc2e023c75527cb13f2bc880dc7fc14b
wangying <wangying@localhost.localdomain>
2017-03-19 22:59:34 +0800

    modified:   conf_to_csv.py

--- tools/configurator/conf_to_csv.py
+++ tools/configurator/conf_to_csv.py
@@ -8,47 +8,25 @@ import os
 import shutil
 import csv
 
-def main():
-	convert()
-
-def convert(root):
+def convert():
 	src= './trasev.config'
+	today = date.today()
+	dest = "stra_set" + today.strftime("%Y%m%d") + ".csv"
+
 	tree = ET.parse(src)
 	root = tree.getroot()
-	strategies = root.find("./strategies")
-	# find a strategy element as template
-	strategy_temp = strategies.find("./strategy")
+	with open(dest,'w') as f:
+		f.write("{0},{1},{2},{3}\n".format("id", "strategy","contract", "lot")) 
+		for straEn in root.findall('.//strategy'):
+			stra_id = straEn.get('id')
+			stra_name = straEn.get('model_file')
+			symbol = straEn.find("./symbol")
+			contract = symbol.get("name")
+			lot = int(symbol.get("max_pos"))
+			f.write("{0},{1},{2},{3}\n".format(stra_id,stra_name,contract,lot))
 
-	# skip the first row, title row
-	dest = 'stra_settings.csv'
-	with open(stra_setting) as csvfile:
-		reader = csv.reader(csvfile)
-		for row in reader:
-			strategy_tmp_str = ET.tostring(strategy_temp, encoding="utf-8")
-			new_strategy = ET.fromstring(strategy_tmp_str)
-			strategies.append(new_strategy);
-			new_strategy.set('id', str(id)) 
-			new_strategy.set('model_file', row[1]) 
-			symbol = new_strategy.find("./symbol")
-			symbol.set('max_pos', row[3])
-			symbol.set('name', row[2])
-
-def clear(root):
-	'''
-	removes strategy elements from trasev.config until there is 
-	a strategy element left.
-	'''
-	stras = root.findall("./strategies/strategy")
-	stra_cnt = len(stras);
-	if stra_cnt > 1:
-		del stras[0]
-	elif stra_cnt == 0:
-		raise Exception('There is NOT one strategy element!')
-
-	strategies_e = root.find("./strategies")
-	for stra in stras:
-		strategies_e.remove(stra)
-
+def main():
+	convert()
 
 if __name__=="__main__":   
 	main()
